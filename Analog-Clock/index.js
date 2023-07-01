62840f02cdbe974e4e766110262a599c69fd274f
udaykiran1012 <123919244+udaykiran1012@users.noreply.github.com>
2023-07-01 21:53:32 +0530

    project using html css js

---
+++ Analog-Clock/index.js
@@ -0,0 +1,36 @@
+const body = document.querySelector('body'),
+hourHand = document.querySelector('.hour'),
+minuteHand = document.querySelector('.minute'),
+secondHand = document.querySelector('.second'),
+modeSwitch = document.querySelector('.mode-switch');
+
+
+if(localStorage.getItem("mode")=== "Dark Mode"){
+    body.classList.add("dark");
+    modeSwitch.textContent = "Light Mode"
+}
+modeSwitch.addEventListener('click',()=>{
+    body.classList.toggle('dark');
+
+    const isdarkmode = body.classList.contains("dark");
+    modeSwitch.textContent = isdarkmode ? "Light Mode" : "Dark Mode";
+
+    localStorage.setItem("mode",isdarkmode ? "Dark Mode" : "Light Mode");
+
+})
+
+const updateTime = () =>{
+    let date = new Date(),
+    secToDeg = (date.getSeconds() /60) * 360;
+    minToDeg = (date.getMinutes() /60) * 360;
+    hourToDeg = (date.getHours() /12) * 360;
+
+    secondHand.style.transform = `rotate(${secToDeg}deg)`;
+    minuteHand.style.transform = `rotate(${minToDeg}deg)`;
+    hourHand.style.transform = `rotate(${hourToDeg}deg)`;
+
+
+}
+
+setInterval(updateTime,1000);
+updateTime();
